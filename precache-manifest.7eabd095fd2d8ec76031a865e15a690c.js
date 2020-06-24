6b571fd6785eeac222ced8a2d011dffdafc5671c
AppVeyor <ci@appveyor.com>
2020-06-24 12:20:10 +0000

    b8f732e

---
+++ precache-manifest.7eabd095fd2d8ec76031a865e15a690c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ebac8cb7f0133d22ab4f8f7f8759a5",
+    "url": "/RA_redux_editing/index.html"
+  },
+  {
+    "revision": "9c36db9170b7fbfa12d2",
+    "url": "/RA_redux_editing/static/css/main.c53f8a68.chunk.css"
+  },
+  {
+    "revision": "0e5a0b463f23ad781c32",
+    "url": "/RA_redux_editing/static/js/2.66488c88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RA_redux_editing/static/js/2.66488c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c36db9170b7fbfa12d2",
+    "url": "/RA_redux_editing/static/js/main.a240b5cf.chunk.js"
+  },
+  {
+    "revision": "1c5c6a54755aee229eac",
+    "url": "/RA_redux_editing/static/js/runtime-main.43010768.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/RA_redux_editing/precache-manifest.7eabd095fd2d8ec76031a865e15a690c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RA_redux_editing/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
